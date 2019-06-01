859ffea2c05de0b9d0cdfc49094a0768ca990eb7
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-06-01 13:57:34 +0530

    tabsets

---
+++ tabsets/server.R
@@ -0,0 +1,23 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    
+    output$myhist<-renderPlot({
+        colm<-as.numeric(input$variable)
+        hist(iris[,colm],breaks=seq(0,max(iris[,colm]),
+                                    l=input$bins+1),col=input$color,
+             main="histogram of iris dataset",xlab = names(iris[colm]))
+        
+    })
+})
