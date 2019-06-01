ed27b1486a72a47131328964c270c7726d114f53
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-06-01 13:14:53 +0530

    textinputwidget

---
+++ textinputwidget/ui.R
@@ -0,0 +1,29 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Demonstration of textinput() widget."),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(("Enter your Personal Information"),
+            textInput("name","enter your name",""),
+            textInput("age","Enter your age","")),
+
+        # Show a plot of the generated distribution
+        mainPanel(("Personal information" ),
+                  textOutput("myname"),
+                  textOutput("myage"))
+    )
+))
