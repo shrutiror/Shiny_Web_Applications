137a9a84368a6ee0f8ed9612d01be0dc96072585
shrutiror <45755921+shrutiror@users.noreply.github.com>
2019-06-01 13:53:05 +0530

    submitbutton1

---
+++ submitbutton1/server.R
@@ -0,0 +1,19 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    
+ output$text1<-renderText(paste("the first name is:" ,input$var1))
+ output$text2<-renderText(paste("The last name is:" ,input$var2))
+
+})
---
+++ submitbutton1/ui.R
@@ -0,0 +1,36 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Demonstration of submitbutton()"),
+
+    sidebarLayout(
+        sidebarPanel(
+            textInput("var1",
+                        "Enter your first name:"
+                        ),
+            textInput("var2",
+                      "Enter your last name:"
+            ),
+            submitButton("Update"),
+            p("click on the update button to see the first name and last name you entered")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            textOutput("text1"),
+            textOutput("text2")
+        )
+    )
+))
